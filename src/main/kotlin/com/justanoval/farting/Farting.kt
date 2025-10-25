cfa4516466d5424da46dca8956ee4c7f2018dbc2
justanoval <justanoval@gmail.com>
2025-10-25 12:54:12 -0700

    added a reload command for config

--- src/main/kotlin/com/justanoval/farting/Farting.kt
+++ src/main/kotlin/com/justanoval/farting/Farting.kt
@@ -5,6 +5,9 @@ import eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils
 import me.fzzyhmstrs.fzzy_config.api.ConfigApi
 import me.fzzyhmstrs.fzzy_config.api.RegisterType
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback
+import net.minecraft.server.command.CommandManager.literal
+import net.minecraft.text.Text
 import net.minecraft.util.Identifier
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -27,5 +30,19 @@ class Farting : ModInitializer {
     override fun onInitialize() {
         PolymerResourcePackUtils.addModAssets(MOD_ID)
         PolymerResourcePackUtils.markAsRequired()
+
+        // reload config command
+        CommandRegistrationCallback.EVENT.register { dispatcher, _, _ ->
+            dispatcher.register(literal("farting")
+                .requires { source -> source.hasPermissionLevel(3)}
+                .then(literal("reload")
+                    .executes { context ->
+                        CONFIG = ConfigApi.registerAndLoadConfig(::FartingConfig, RegisterType.SERVER)
+                        context.source.sendFeedback({ Text.literal("Reloaded Farting config!") }, true)
+                        1
+                    }
+                )
+            )
+        }
     }
 }
